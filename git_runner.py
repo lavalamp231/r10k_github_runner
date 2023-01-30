ce341825f97776caf9e2ceedfbd05ac9b6d8dfcc
root <root@puppetserver01.esxi.com>
2023-01-29 21:28:43 -0600

    updated variables

--- git_runner.py
+++ git_runner.py
@@ -9,29 +9,29 @@ puppet_environment_dir = '/etc/puppetlabs/code/environments/'
 log_files = '/var/log/r10k/r10k_runner.txt'
 
 def repo_change():
-    my_repo = git.Repo(control_repo)
-    puppet_environments = os.listdir(puppet_environment_dir)
-    for branch in my_repo.branches:
+    my_repo = git.Repo(control_repo) # intialiazing repo
+    puppet_environments = os.listdir(puppet_environment_dir) # variable for list of directories in /etc/puppetlabs/code/environments
+    for branch in my_repo.branches: # loop to get every branch in my repo (for what ever reason it's not showing all branches - trying to figure shit out)
       #print(branch)
-      my_repo.git.checkout(branch)
+      my_repo.git.checkout(branch) # checking out each branch
       #my_repo.fetch()
-      a = len(list(my_repo.iter_commits(branch)))
+      commit_count = len(list(my_repo.iter_commits(branch)))
       try:
         my_repo.git.pull()
       except:
         os.system(f"git branch --set-upstream-to=origin/{branch} {branch}")
       if branch not in repo_dict:
-        repo_dict[branch] = {'Length': a}
-        l = repo_dict[branch]['Length']
-        print(f"list length: {a} in {branch}")
-        print(f"length in dict: {l} in {branch}")
+        repo_dict[branch] = {'Length': commit_count}
+        length = repo_dict[branch]['Length']
+        print(f"list length: {commit_count} in {branch}")
+        print(f"length in dict: {length} in {branch}")
       else:
-        l = repo_dict[branch]['Length']
-        if repo_dict[branch]['Length'] < a:
+        length = repo_dict[branch]['Length']
+        if length < commit_count:
           print(f"{branch} has changed")
           os.system(f"/usr/bin/r10k deploy environment {branch} && chmod 775 /etc/puppetlabs/code/environments/production/scripts/config_version.sh && chown -R puppet:puppet /etc/puppetlabs/code/")
           print(f"r10k creating {branch}")
-          repo_dict[branch] = {'Length': a}
+          repo_dict[branch] = {'Length': commit_count}
 
         if str(branch) not in puppet_environments:
           os.system(f"/usr/bin/r10k deploy environment {branch}")

965b1d860b77f024eb3b9f990216ee44481f1e80
root <root@puppetserver01.esxi.com>
2023-02-10 20:32:28 -0600

    fixed the issue with it not creating remote branch

--- git_runner.py
+++ git_runner.py
@@ -6,6 +6,7 @@ cwd = os.getcwd()
 control_repo = cwd + '/control-repo'
 git_repo = 'git@github.com:lavalamp231/control-repo.git'
 repo_dict = {}
+repo_list = []
 puppet_environment_dir = '/etc/puppetlabs/code/environments/'
 log_files = '/var/log/r10k/r10k_runner.txt'
 
@@ -17,34 +18,45 @@ def clone_repo():
 def repo_change():
     my_repo = git.Repo(control_repo) # intialiazing repo
     puppet_environments = os.listdir(puppet_environment_dir) # variable for list of directories in /etc/puppetlabs/code/environments
-    for branch in my_repo.branches: # loop to get every branch in my repo (for what ever reason it's not showing all branches - trying to figure shit out)
+    for branch in my_repo.remotes.origin.refs:
+      if branch not in repo_list:
+        repo_list.append(str(branch))
+    for branch in repo_list: # loop to get every branch in my repo (for what ever reason it's not showing all branches - trying to figure shit out)
+      remote_branch = branch.split(" ")[0].replace("origin/", "")
       #print(branch)
-      my_repo.git.checkout(branch) # checking out each branch
-      #my_repo.fetch()
-      commit_count = len(list(my_repo.iter_commits(branch))) # length of the amount of commits in each branch
-      try:
-        my_repo.git.pull()
-      except:
-       os.system(f"git branch --set-upstream-to=origin/{branch} {branch}") # not too sure if this is what I should be doing or not. 
-      if branch not in repo_dict:
-        repo_dict[branch] = {'Length': commit_count}
-        length = repo_dict[branch]['Length']
-        print(f"list length: {commit_count} in {branch}")
-        print(f"length in dict: {length} in {branch}")
-      else:
-        length = repo_dict[branch]['Length']
-        if length < commit_count:
-          print(f"{branch} has changed")
-          os.system(f"/usr/bin/r10k deploy environment {branch} && chmod 775 /etc/puppetlabs/code/environments/{branch}/scripts/config_version.sh && chown -R puppet:puppet /etc/puppetlabs/code/")
-          print(f"r10k creating {branch}")
-          repo_dict[branch] = {'Length': commit_count}
+      if remote_branch != "HEAD":
+        my_repo.git.checkout(remote_branch) # checking out each branch
+        #my_repo.fetch()
+        commit_count = len(list(my_repo.iter_commits(remote_branch))) # length of the amount of commits in each branch
+        try:
+          my_repo.git.pull()
+        except:
+          os.system(f"git branch --set-upstream-to=origin/{remote_branch} {remote_branch}") # not too sure if this is what I should be doing or not. 
+        if remote_branch not in repo_dict:
+          repo_dict[remote_branch] = {'Length': commit_count}
+          length = repo_dict[remote_branch]['Length']
+          print(f"list length: {commit_count} in {remote_branch}")
+          print(f"length in dict: {length} in {remote_branch}")
+        else:
+          length = repo_dict[remote_branch]['Length']
+          if length < commit_count:
+            print(f"{remote_branch} has changed")
+            os.system(f"/usr/bin/r10k deploy environment {remote_branch} && chmod 775 /etc/puppetlabs/code/environments/{remote_branch}/scripts/config_version.sh && chown -R puppet:puppet /etc/puppetlabs/code/")
+            print(f"r10k creating {remote_branch}")
+            repo_dict[remote_branch] = {'Length': commit_count}
 
-        if str(branch) not in puppet_environments:
-          os.system(f"/usr/bin/r10k deploy environment {branch}")
-          print(f"{branch} is not in {puppet_environments}")
-          print(f"r10k creating {branch}")
+          if str(remote_branch) not in puppet_environments:
+            os.system(f"/usr/bin/r10k deploy environment {remote_branch}")
+            print(f"{remote_branch} is not in {puppet_environments}")
+            print(f"r10k creating {remote_branch}")
+          
+          # for remote in repo_list:
+          #   i = remote.split(" ")[0].replace("origin/", "")
+          #   print(i)
 
     time.sleep(5)
+
+
 
 while True:
     clone_repo()
